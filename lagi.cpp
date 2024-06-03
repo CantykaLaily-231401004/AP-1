18ff052760e097168a9ed283a21dee2f357d31bd
CantykaLaily-231401004 <cantykalaily@students.usu.ac.id>
2024-06-03 18:02:24 +0700

    Pertemuan 1: Setup Compiler & Input-Output & Variables

---
+++ lagi.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main () {
+    int a=19;
+    char b='S';
+    float c= 3.14;
+    printf ("%c Merupakan Abjad yang ke- %d", b, a);
+    printf ("Bilangan Phi=%4.2f", c);
+    getch();
+}
